094a91b3b079cc72f02b95749c328b365981924b
Ronaldo Cysne <ronaldocysne@gmail.com>
2022-07-04 13:58:28 -0300

    Todo com mais funções

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let db = [
 ]
 
 
-// Criando o elemento que vai ser dinâmico
+// Criando o elemento com o JS que vai ser dinâmico no DOM
 // Task será o argumento recebido e o status para ver se já está marcado
 const createItem = (task, status='') => {
   const item = document.createElement('label');
@@ -25,4 +25,49 @@ const createItem = (task, status='') => {
   `
   // Adicionando o elemento item ao elemento pai, localizado pelo id
   document.getElementById('todoList').appendChild(item)
+  // Chamando a função ele vai criar o elemento e adicionar o item ao pai
 }
+
+
+const cleanTasks = () => {
+  const todoList = document.getElementById('todoList');
+  // Enquanto existir o primeiro filho, remova um filho, sendo o último filho
+  while(todoList.firstChild) {
+    todoList.removeChild(todoList.lastChild)
+  }
+}
+
+
+// Toda vez que o banco for alterado a tela é mostrada com os dados atualizados
+const updateScreen = () => {
+  // Limpar as tarefas para os dados não serem duplicados
+  cleanTasks();
+  db.forEach(item => createItem(item.tarefa, item.status));
+}
+
+const cleanInputTask = () => {
+  event.target.value = '';
+}
+
+
+const insertItem = (event) => {
+  const key = event.key;
+  // console.log(key);
+  const text = event.target.value
+  
+  if (key === 'Enter') {
+    db.push({'tarefa': text, 'status': ''});
+    updateScreen();
+    cleanInputTask();
+  }
+}
+
+const clickItem = () => {
+  
+}
+
+// evento envia para o callback (insertItem) o event com suas propriedades
+document.getElementById('newItem').addEventListener('keypress', insertItem);
+
+// Atualizar o banco e depois atualizar a tela (Atento para não atualizar só a tela)
+document.getElementById('todoList').addEventListener('click', clickItem);
