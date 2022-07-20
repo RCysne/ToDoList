2bc831a197907695caf004e9c280be91decfcf3a
Ronaldo Cysne <ronaldocysne@gmail.com>
2022-07-19 21:18:46 -0300

    ainda sem o localstorage

--- app.js
+++ app.js
@@ -9,9 +9,19 @@
 
 
 // array para guardar os elementos - Banco de Dados
-let db = [
-  {'tarefa': 'Estudar JS', 'status': '',},
-  {'tarefa': 'Netflix', 'status': 'checked'}
+let banco = [
+  {
+    'tarefa': 'JavaScript',
+    'status': '',
+  },
+  {
+    'tarefa': 'React JS',
+    'status': 'checked',
+  },
+  {
+    'tarefa': 'React Native',
+    'status': '',
+  }
 ];
 
 
@@ -20,81 +30,98 @@ let db = [
 // Criando o elemento com o JS que vai ser dinâmico no DOM
 // Task será o argumento recebido e o status para ver se já está marcado
 // Para a identificação da posição é inserido um data-index para a manipulação
-const createItem = (task, status, index) => { // Função com os parâmetros
+const criarItem = (tarefa, status, indice) => { // Função com os parâmetros
   const item = document.createElement('label'); // Criar o elemento
   item.classList.add('todo__item'); // Adicionar a classe
   // Adicionar o item na DOM
   item.innerHTML = ` 
-  <input type="checkbox" ${status} data-index=${index}/>
-  <div>${task}</div>
-  <input type="button" value="X" data-index=${index}/>
-  `
+  <input type="checkbox" ${status} data-indice=${indice}/>
+  <div>${tarefa}</div>
+  <input type="button" value="X" data-indice=${indice}/>
+  `;
   // Adicionando o elemento filho ao pai
   document.getElementById('todoList').appendChild(item)
 }
 
 // =================== Deletando =========================================================================
 
-// Para limpar os dados e não repetir
-const cleanTasks = () => {
+// Para NÃO REPETIR OS DADOS quando a função for chamada
+const limparTarefas = () => {
   const todoList = document.getElementById('todoList');
-  
-  // Enquanto existir o primeiro filho, remova um filho, sendo ele o último
-  while(todoList.firstChild) {
-    todoList.removeChild(todoList.lastChild)
+
+  // Enquanto existir um filho, remova um filho, sendo ele o último
+  while (todoList.firstChild) {
+    todoList.removeChild(todoList.lastChild);
   }
 }
 
 // ================== Atualizar a tela depois de qualquer alteração =======================================
 
 // Toda vez que o banco for alterado a tela é mostrada com os dados atualizados
-const updateScreen = () => {
+const atualizarTela = () => {
   // Limpar as tarefas para os dados não serem duplicados
-  cleanTasks();
-  db.forEach((item, index) => createItem(item.tarefa, item.status, index));
+  limparTarefas();
+  banco.forEach((item, indice) => criarItem(item.tarefa, item.status, indice));
 }
 
 // ===================== Limpando o input ==================================================================
-const cleanInputTask = () => {
-  event.target.value = '';
+const limparInput = (evento) => {
+  evento.target.value = '';
 }
 
 
 // =============== Inserindo o item no array, atualizando o array e limpando os inputs =====================
 
-const insertItem = (event) => {
-  const key = event.key;
+const inserirItem = (evento) => {
+  const tecla = evento.key;
   // console.log(key);
-  const text = event.target.value
-  
-  if (key === 'Enter') {
-    db.push({'tarefa': text, 'status': ''});
-    updateScreen();
-    cleanInputTask();
+  const texto = evento.target.value
+
+  if (tecla === 'Enter') {
+    banco.push({ 'tarefa': texto, 'status': '' });
+    atualizarTela();
+    limparInput();
   }
 }
 
 
 // ========================== Remoção do item ==============================================================
 
-const removeItem =(indexNumber) => {
-  db.splice(indexNumber, 1);
-  // updateScreen();
+
+
+// Removeu o item do banco e atualizou a tela com os dados atualizados do banco
+const removerItem = (indice) => {
+  banco.splice(indice, 1);
+  atualizarTela();
 }
 
-// Clicando, capturando o item e recebendo seu index para a remoção d index escolhido
-const clickItem = (event) => {
-  const element = event.target
-  console.log(element)
-  if(element.type === "button") {
-    const indexNumber = element.dataset.index;
-    console.log(indexNumber)
-    removeItem(indexNumber)
+const atualizarItem = (indice) => {
+  banco[indice].status = banco[indice].status === '' ? 'checked' : '';
+  atualizarTela();
+}
+
+// Clicando, capturando o item e recebendo seu index para a remoção do index escolhido
+// dataset é a propriedade que captura o dado colocado quando se usa o data-nome
+const clickItem = (evento) => {
+  const elemento = evento.target;
+
+  // Se o element for igual ao button, eu removo o elemento
+  if (elemento.type === "button") {
+    const indice = elemento.dataset.indice;
+
+    removerItem(indice)
+
+    // Se o element for igual ao checkbox eu atualizo inserindo o checkbox no db na tarefa
+  } else if (elemento.type === 'checkbox') {
+    const indice = elemento.dataset.indice;
+    atualizarItem(indice);
   }
 }
 
 // evento envia para o callback (insertItem) o event com suas propriedades
-document.getElementById('newItem').addEventListener('keypress', insertItem);
+document.getElementById('newItem').addEventListener('keypress', inserirItem);
 
 // Atualizar o banco e depois atualizar a tela (Atento para não atualizar só a tela)
+// Pegando a div para reconhecer com o clickItem qual elemento estou clicando 
 document.getElementById('todoList').addEventListener('click', clickItem);
+atualizarTela();
